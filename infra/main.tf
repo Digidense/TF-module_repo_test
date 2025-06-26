b05105c08a4f3bb1562048512e6fcac9a3ba6f7f
asobin <asobin@gmail.com>
2025-06-26 13:56:44 +0530

    feature/jira-123-creating-tf-module

--- infra/main.tf
+++ infra/main.tf
@@ -6,6 +6,7 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_subnet" "subnet1" {
+  count = 0
   vpc_id                  = var.vpc_id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
@@ -16,6 +17,7 @@ resource "aws_subnet" "subnet1" {
   }
 }
 resource "aws_subnet" "subnet2" {
+  count = 0
   vpc_id                  = var.vpc_id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
